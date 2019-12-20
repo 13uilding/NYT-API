66a1dac7f129c7cd53e39fe94154615a0a098dea
deebs <hereisdanbrown@gmail.com>
2019-12-20 14:07:38 -0700

    Finished the targets

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-
-var search = $("#searchTerm");
 // Need to add this functionality
-var records = $("#numRecords");
-var sYear = "&begin_date=" + $("#startYear") + "0101";
-var eYear = "&end_date=" + String($("#endYear")) + "1231";
+var records = $("#numRecords").val().trim();
+// Input fields
+var search = $("#searchTerm").val().trim();
+var sYear = "&begin_date=" + String($("#startYear")).val().trim() + "0101";
+var eYear = "&end_date=" + String($("#endYear")).val().trim() + "1231";
+// Buttons
 var searchBtn = $(".btn-outline-success");
 var clearBtn = $(".btn-outline-danger");
-var topArticles = $("topArticles")
-
+// Div
+var topArticles = $("#topArticles")
 
 var apiKey = "D3EAWzjhRflruhSt5r8LbpFpp8yuV3VA"
 var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${search}${sYear}${eYear}&api-key=${apiKey}`
@@ -15,7 +16,7 @@ var queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${sea
 
 $.ajax({
     url: queryURL,
-    method: "get"
+    method: "GET"
 }).then(function(response){
     console.log(response);
 })
